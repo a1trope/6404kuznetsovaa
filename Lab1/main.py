e2b10068fe60f06bab92f84fd0bdd54d8825d8f3
a1trope <bigtinyturtle@gmail.com>
2024-09-16 19:44:47 +0400

    wip

--- Lab1/main.py
+++ Lab1/main.py
@@ -4,18 +4,19 @@ import json
 import parser
 
 
-# def f(x, a, b, c):
-#     return a - math.cos(b * x + c) ** 2
-#
-#
-# config = parser.ParserJSON().parse("config.json")
-#
-# result = {}
-# for x in range(config.n0, config.nk + config.h, config.h):
-#     result[x] = f(x, config.a, config.b, config.c)
-#
-#
-# with open("results.json", "w") as f:
-#     json.dump(result, f)
+def f(x, a, b, c):
+    return a - math.cos(b * x + c) ** 2
 
-print(sys.argv[1:])
+
+if len(sys.argv) > 1:
+    config = parser.Config(*map(int, sys.argv[1:]))
+else:
+    config = parser.ParserJSON().parse("config.json")
+
+result = {}
+for x in range(config.n0, config.nk + config.h, config.h):
+    result[x] = f(x, config.a, config.b, config.c)
+
+
+with open("results.json", "w") as f:
+    json.dump(result, f)
